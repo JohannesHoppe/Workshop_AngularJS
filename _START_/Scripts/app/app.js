cdbcc7fd43d6f8e74d59377ff3d7ca1b2b79c503
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2015-06-18 00:37:23 +0200

    starter files

---
+++ _START_/Scripts/app/app.js
@@ -0,0 +1,19 @@
+﻿define([
+    'require',
+    'angular',
+    'angular-route'
+    ], function(require, angular) {
+
+    angular.module('app', ['ngRoute'])
+        .config(['$routeProvider', function($routeProvider) {
+
+
+            }
+        ]);
+
+
+    // bootstrap Angular after require.js and DOM are ready
+    angular.element(document).ready(function () {
+        angular.bootstrap(document, ['app']);
+    });
+});
